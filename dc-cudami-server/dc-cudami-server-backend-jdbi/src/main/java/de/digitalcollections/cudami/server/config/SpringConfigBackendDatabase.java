9bcf70ff68c9d87dea11f2c4516705402b777ef0
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-12-30 18:59:59 +0100

    Add jooq code generation and spring config

--- dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/SpringConfigBackendDatabase.java
+++ dc-cudami-server/dc-cudami-server-backend-jdbi/src/main/java/de/digitalcollections/cudami/server/config/SpringConfigBackendDatabase.java
@@ -9,22 +9,41 @@ import javax.sql.DataSource;
 import org.jdbi.v3.postgres.PostgresPlugin;
 import org.jdbi.v3.spring4.JdbiFactoryBean;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /** Database configuration. */
 @Configuration
 @ComponentScan(basePackages = {"de.digitalcollections.cudami.server.backend.impl.jdbi"})
+@EnableTransactionManagement
 public class SpringConfigBackendDatabase {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SpringConfigBackendDatabase.class);
+
+  @Value("${spring.datasource.url}")
+  String datasourceUrl;
+
+  @Value("${spring.datasource.username}")
+  String datasourceUsername;
+
+  @Value("${spring.datasource.password}")
+  String datasourcePassword;
+
+  @Autowired private DataSource dataSource;
 
   @Autowired
   @Bean
@@ -45,5 +64,55 @@ public class SpringConfigBackendDatabase {
     plugins.add(new JsonbJdbiPlugin(objectMapper));
     jdbiFactoryBean.setPlugins(plugins);
     return jdbiFactoryBean;
+  }
+
+  @Bean
+  public DataSourceConnectionProvider connectionProvider() {
+    return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(dataSource));
+  }
+
+  @Bean
+  public DefaultDSLContext dsl() {
+    return new DefaultDSLContext(configuration());
+  }
+
+  @Bean
+  public ExceptionTranslator exceptionTransformer() {
+    return new ExceptionTranslator();
+  }
+
+  public DefaultConfiguration configuration() {
+    //    org.jooq.meta.jaxb.Configuration configuration =
+    //        new org.jooq.meta.jaxb.Configuration()
+    //            .withJdbc(
+    //                new Jdbc()
+    //                    .withDriver("org.postgresql.Driver")
+    //                    .withUrl(datasourceUrl)
+    //                    .withUser(datasourceUsername)
+    //                    .withPassword(datasourcePassword))
+    //            .withGenerator(
+    //                new Generator()
+    //                    .withDatabase(
+    //                        new Database()
+    //                            .withName("org.jooq.meta.postgres.PostgresDatabase")
+    //                            .withIncludes(".*")
+    //                            .withExcludes("")
+    //                            .withInputSchema("public"))
+    //                    .withTarget(
+    //                        new Target()
+    //                            .withPackageName(
+    //                                "de.digitalcollections.cudami.server.backend.impl.jooq")
+    //                            .withDirectory("target/generated-sources/jooq")));
+    //
+    //    try {
+    //      GenerationTool.generate(configuration);
+    //    } catch (Exception ex) {
+    //      LOGGER.error("JOOQ code generation failed!", ex);
+    //    }
+
+    DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+    jooqConfiguration.set(connectionProvider());
+    jooqConfiguration.set(new DefaultExecuteListenerProvider(exceptionTransformer()));
+    return jooqConfiguration;
   }
 }
